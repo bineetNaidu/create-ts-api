47a79b6a0714f4e2a2d3e3495b1b464367578820
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-27 15:17:10 +0530

    🎉 add Mongoose Connections

--- packages/templates/gql-mongo/src/index.ts
+++ packages/templates/gql-mongo/src/index.ts
@@ -1,13 +1,27 @@
 import 'reflect-metadata';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 import { ApolloServer } from 'apollo-server';
 import { buildSchema } from 'type-graphql';
 import { HelloResolver } from './resolvers/Hello';
+import { TweetResolver } from './resolvers/TweetResolver';
 
+dotenv.config();
 const bootstrap = async () => {
+  if (!process.env.MONGO_URI) {
+    throw new Error('??>> {" MONGO_URI must be defined!! "} ');
+  }
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  });
+
   const server = new ApolloServer({
     schema: await buildSchema({
       validate: false,
-      resolvers: [HelloResolver],
+      resolvers: [HelloResolver, TweetResolver],
     }),
   });
 
@@ -16,4 +30,7 @@ const bootstrap = async () => {
   });
 };
 
-bootstrap();
+bootstrap().catch((error) => {
+  console.error(error.message);
+  process.exit(1);
+});
