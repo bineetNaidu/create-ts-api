d0636356ed215af3a1b3658578ab494aec91b15d
Bineet Naidu <bineet.s.naidu@gmail.com>
2026-08-02 01:17:29 +0530

    feat(template-gql-mongo): add DatabaseError handler and CatchMongoErrors middleware (#30)

---
+++ packages/templates/gql-mongo/src/lib/errors/ValidationError.ts
@@ -0,0 +1,10 @@
+import { AppError } from './AppError.js';
+
+export class ValidationError extends AppError {
+  constructor(
+    message: string = 'Invalid input arguments',
+    validationErrors?: Record<string, unknown> | unknown[],
+  ) {
+    super(message, 'BAD_USER_INPUT', 400, { validationErrors });
+  }
+}
