7370519ac8ab83ecd2e091dac197d036cd7c742a
Bineet Naidu <bineet.s.naidu@gmail.com>
2026-08-02 15:31:27 +0530

    refactor(template-gql): cleanup redundant type constituents in testServer

--- packages/templates/gql/src/test/testServer.ts
+++ packages/templates/gql/src/test/testServer.ts
@@ -4,29 +4,29 @@ import { buildSchema } from 'type-graphql';
 import { HelloResolver } from '@/modules/Hello/hello.resolver.js';
 
 export const createTestServer = async () => {
-  return new ApolloServer({
-    schema: await buildSchema({
-      resolvers: [HelloResolver],
-      validate: true,
-    }),
-  });
+	return new ApolloServer({
+		schema: await buildSchema({
+			resolvers: [HelloResolver],
+			validate: true,
+		}),
+	});
 };
 
 /**
  * Helper to execute GraphQL queries in tests easily
  */
 export const executeGraphQL = async (
-  server: ApolloServer,
-  query: string,
-  variables?: any | Record<string, unknown>,
+	server: ApolloServer,
+	query: string,
+	variables: Record<string, unknown> = {},
 ) => {
-  const response = await server.executeOperation({
-    query,
-    variables,
-  });
+	const response = await server.executeOperation({
+		query,
+		variables,
+	});
 
-  if (response.body.kind === 'single') {
-    return response.body.singleResult;
-  }
-  throw new Error('Unexpected multipart GraphQL response in test');
+	if (response.body.kind === 'single') {
+		return response.body.singleResult;
+	}
+	throw new Error('Unexpected multipart GraphQL response in test');
 };
